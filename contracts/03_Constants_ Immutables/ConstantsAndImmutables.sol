34af4b2cd02e78c145976a3d2ac294a3072487bf
Azat Sayan <sayanazat@gmail.com>
2022-07-30 14:18:59 +0300

    Constants and Immutables

---
+++ contracts/03_Constants_ Immutables/ConstantsAndImmutables.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract ConstantsAndImmutables {
+    /**-----------------Constants------------------- */
+    /**
+    Constants 
+      - They are variables that cannot be modified.
+      - Their value is hard coded and using constants can save gas cost. 
+      */
+    address public constant MY_CONSTANT_ADDRESS =
+        0x14dC79964da2C08b23698B3D3cc7Ca32193d9955;
+    uint256 public constant MY__CONSTANT_UINT = 1234;
+
+    /**-----------------Immutables------------------- */
+    /**
+    Immutables
+      - Immutables variables are like constants. 
+      - Values of immutable variables can be set inside the constructor but cannot be modified afterwards.
+    */
+
+    address public immutable MY_IMMUTABLE_ADDRESS;
+    uint256 public immutable MY_IMMUTABLE_UINT;
+
+    constructor(uint256 _myUint) {
+        MY_IMMUTABLE_ADDRESS = msg.sender;
+        MY_IMMUTABLE_UINT = _myUint;
+    }
+}
