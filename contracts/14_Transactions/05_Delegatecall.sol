3aaf56fc504c2244995740911fef1b5bf79eac2f
Azat Sayan <sayanazat@gmail.com>
2022-09-12 15:01:55 +0300

    Transactions

--- contracts/14_Transactions/05_Delegatecall.sol
+++ contracts/14_Transactions/05_Delegatecall.sol
@@ -27,4 +27,11 @@ contract A {
     uint256 public num;
     address public sender;
     uint256 public value;
+
+    function setVars(address _contract, uint256 _num) public payable {
+        // A's storage is set, B is not modified.
+        (bool success, bytes memory data) = _contract.delegatecall(
+            abi.encodeWithSignature("setVars(uint256)", _num)
+        );
+    }
 }
